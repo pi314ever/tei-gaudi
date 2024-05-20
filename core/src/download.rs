a7ee6f1fe13ce8aed440d0baa0953cd8f5c23e0a
Daniel Huang <pilotflyer824@gmail.com>
2024-05-20 02:30:36 -0700

    Model download enhancing (#11)

--- core/src/download.rs
+++ core/src/download.rs
@@ -1,4 +1,7 @@
-use hf_hub::api::tokio::{ApiError, ApiRepo};
+use hf_hub::api::{
+    tokio::{ApiError, ApiRepo},
+    RepoInfo,
+};
 use std::path::PathBuf;
 use tracing::instrument;
 
@@ -13,27 +16,55 @@ pub const ST_CONFIG_NAMES: [&str; 7] = [
     "sentence_xlnet_config.json",
 ];
 
+/// Parses a [`RepoInfo`] object for model weight files to download. Returns a non-empty vector of
+/// model files that contains a model type str, if there are any.
+#[instrument(skip_all)]
+fn _weight_files_to_download<'a>(
+    api_info: &'a RepoInfo,
+    weight_file_type_str: &'a str,
+) -> Option<Vec<&'a str>> {
+    let ignored_file_contains = ["arguments", "args", "training", "medusa_lm_head"];
+    let files: Vec<&str> = api_info
+        .siblings
+        .iter()
+        .map(|s| s.rfilename.as_str())
+        .filter(|f| {
+            f.contains(weight_file_type_str)
+                && f.split("/").count() == 1
+                && ignored_file_contains.iter().all(|s| !f.contains(s))
+        })
+        .collect();
+    if files.is_empty() {
+        return None;
+    }
+    Some(files)
+}
+
 #[instrument(skip_all)]
 pub async fn download_artifacts(api: &ApiRepo) -> Result<PathBuf, ApiError> {
     let start = std::time::Instant::now();
 
     tracing::info!("Starting download");
 
-    api.get("config.json").await?;
-    api.get("tokenizer.json").await?;
-
-    let model_root = match api.get("model.safetensors").await {
-        Ok(p) => p,
-        Err(_) => {
-            let p = api.get("pytorch_model.bin").await?;
-            tracing::warn!("`model.safetensors` not found. Using `pytorch_model.bin` instead. Model loading will be significantly slower.");
-            p
-        }
-    }
+    let model_root = api
+        .get("config.json")
+        .await?
         .parent()
         .unwrap()
         .to_path_buf();
+    api.get("tokenizer.json").await?;
 
+    let api_info = api.info().await?;
+    let model_files = _weight_files_to_download(&api_info, "safetensors")
+        .or_else(|| {
+            tracing::warn!("`model.safetensors` not found. Using `pytorch_model.bin` instead. Model loading will be significantly slower.");
+            _weight_files_to_download(&api_info, "pytorch_model")
+        }).expect("No model files found as `safetensors` or `pytorch_model`");
+
+    // Download the model files
+    for file_name in model_files {
+        api.get(file_name).await?;
+    }
     tracing::info!("Model artifacts downloaded in {:?}", start.elapsed());
     Ok(model_root)
 }
